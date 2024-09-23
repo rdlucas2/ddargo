6e6c36bb7cfec127c2c5d5537a6b3a5e1c6dfabf
Ryan Lucas <1649129+rdlucas2@users.noreply.github.com>
2024-09-22 20:12:19 -0400

    add ingress

--- app-src/goapp1/main.go
+++ app-src/goapp1/main.go
@@ -12,8 +12,20 @@ func generateUUIDHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Random UUID: %s", randomUUID)
 }
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK")
+}
+
+func readyHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Ready")
+}
+
 func main() {
 	http.HandleFunc("/", generateUUIDHandler)
+	http.HandleFunc("/healthz", healthzHandler)
+	http.HandleFunc("/ready", readyHandler)
 
 	fmt.Println("Server starting on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
