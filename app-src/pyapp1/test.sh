7c66dd604e3cbff834be2130ed4f42542fcdee2a
Ryan Lucas <1649129+rdlucas2@users.noreply.github.com>
2024-09-29 11:37:03 -0400

    add comment to sed command for test.sh

--- app-src/pyapp1/test.sh
+++ app-src/pyapp1/test.sh
@@ -6,4 +6,5 @@ mkdir -p /app/out
 # Run pytest with coverage and output results to /app/out
 pytest --cov=./ --cov-report=xml:/app/out/coverage.xml --junitxml=/app/out/tests.xml
 
+# if running in container, update the source in coverage file so it aligns
 sed -i 's|<source>/app|<source>.|g' /app/out/coverage.xml
